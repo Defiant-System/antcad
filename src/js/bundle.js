fd0955a9ffc6105e494c1d19c79cf435be6d9cd3
Hakan Bilgin <hbi99@hotmail.com>
2020-09-14 01:38:45 +0200

    implemented bundle.min feature

---
+++ src/js/bundle.js
@@ -0,0 +1,15 @@
+
+import * as THREE from "./modules/three.module.js";
+import { OrbitControls } from "./modules/OrbitControls.js";
+import { EffectComposer } from './modules/postprocessing/EffectComposer.js';
+import { RenderPass } from './modules/postprocessing/RenderPass.js';
+import { OutlinePass } from './modules/postprocessing/OutlinePass.js';
+
+export {
+	THREE,
+	OrbitControls,
+	EffectComposer,
+	RenderPass,
+	OutlinePass,
+};
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 
-import * as THREE from "./modules/three.module.js";
-import { OrbitControls } from "./modules/OrbitControls.js";
-import { EffectComposer } from './modules/postprocessing/EffectComposer.js';
-import { RenderPass } from './modules/postprocessing/RenderPass.js';
-import { OutlinePass } from './modules/postprocessing/OutlinePass.js';
+import {
+	THREE,
+	OrbitControls,
+	EffectComposer,
+	RenderPass,
+	OutlinePass,
+} from './bundle.min.js';
 
 
 let camera,
